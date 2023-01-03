dab0047aa72985fd598e58861ceb50a30f7d8983
river <river@DESKTOP-88UT4CN>
2023-01-02 22:35:03 -0500

    Agregar archivos de proyecto.

---
+++ UISRAEL/Login.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UISRAEL
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Login : ContentPage
+	{
+		public Login ()
+		{
+			InitializeComponent ();
+		}
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "irivera";
+            string contraseña = "12345";
+
+            if (usuario == txtUsuario.Text && contraseña == txtContraseña.Text)
+            {
+                Navigation.PushAsync(new Principal(usuario));
+            }
+            else
+            {
+                DisplayAlert("Alerta", "Usuario/Contraseña Incorrecta", "Cerrar");
+            }
+        }
+    }
+}
---
+++ UISRAEL/Page1.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UISRAEL
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcularP1_Clicked(object sender, EventArgs e)
+        {
+            double nSeguimiento = Convert.ToDouble(txtNSeguimiento.Text);               
+            double nExamen = Convert.ToDouble(txtNExamen.Text);
+
+            double parcial1 = (nSeguimiento * 0.3) + (nExamen * 0.2);
+            txtResultadoP1.Text = parcial1.ToString();
+        }
+
+        private void btnCalcularP2_Clicked(object sender, EventArgs e)
+        {
+            double nSeguimiento2 = Convert.ToDouble(txtNSeguimiento2.Text);
+            double nExamen2 = Convert.ToDouble(txtNExamen2.Text);
+
+            double parcial2 = (nSeguimiento2 * 0.3) + (nExamen2 * 0.2);
+            txtResultadoP2.Text = parcial2.ToString();
+        }
+
+        private void btnCalcularNF_Clicked(object sender, EventArgs e)
+        {
+            double resultadop1 = Convert.ToDouble(txtResultadoP1.Text);
+            double resultadop2 = Convert.ToDouble(txtResultadoP2.Text);
+
+            double notafinal = resultadop1 + resultadop2;
+            txtNotaFinal.Text = notafinal.ToString();
+
+            double notafinalif = Convert.ToDouble(txtNotaFinal.Text);
+
+            if (notafinalif < 5)
+
+            {
+                DisplayAlert("ESTADO", "REPROBADO", "Cerrar");
+            }
+
+            if (notafinalif <= 7)
+
+            {
+                return;
+            }
+            DisplayAlert("ESTADO", "APROBADO", "Cerrar");
+
+        }
+    }
+}
